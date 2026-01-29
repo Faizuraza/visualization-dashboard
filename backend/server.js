75a4c793d8592f51626d773346bb918786f712e2
Faiz Raza <faizuraza007@gmail.com>
2026-01-29 20:28:49 +0530

    Fix Conditional detevn loading for production

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,8 @@
 if (process.env.NODE_ENV !== 'production') {
     require("dotenv").config();
 }
-
 const app = require("./app");
 const connectDB = require("./config/db");
-
 connectDB();
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
