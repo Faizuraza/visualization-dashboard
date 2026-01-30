d0b20d15e9e86dc5c2c9325cfdf775fdbd84038e
Faiz Raza <faizuraza007@gmail.com>
2026-01-31 02:26:21 +0530

    Remove filter icon from filter panel header

--- frontend/src/components/filters/FilterPanel.jsx
+++ frontend/src/components/filters/FilterPanel.jsx
@@ -53,9 +53,6 @@ const FilterPanel = () => {
       <div className="px-3 py-3 border-b border-white/10">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clipRule="evenodd" />
-            </svg>
             <h2 className="text-sm font-bold text-white">
               Filters
             </h2>
